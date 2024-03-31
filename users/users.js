c6853e6d69d9fbd02a266918b9fadc6030bbf3b5
AndriyTok <andriy.tokovyy@gmail.com>
2024-03-31 11:44:37 +0300

    First page's style done✔️

--- users/users.js
+++ users/users.js
@@ -1,13 +1,15 @@
 const container = document.createElement('div');
-
+container.classList.add('container')
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(users => users.json())
     .then((users) => {
         users.forEach(user => {
             const user_div = document.createElement('div');
+            user_div.classList.add('user')
             user_div.innerHTML = `<p> ID: ${user.id}<br> Name: ${user.name}</p>`;
 
             const button = document.createElement('button');
+            button.classList.add('reddit-mono')
             button.innerText = 'More';
             button.addEventListener('click', ()=> {
                 localStorage.setItem('selected_user', JSON.stringify(user));
