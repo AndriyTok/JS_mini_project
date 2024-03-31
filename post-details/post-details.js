6e4d78f4ce84e38c4e952c3fb21d2f089a3e8521
AndriyTok <andriy.tokovyy@gmail.com>
2024-03-31 22:41:16 +0300

    Everything done✔️

--- post-details/post-details.js
+++ post-details/post-details.js
@@ -1,7 +1,8 @@
 const post = JSON.parse(localStorage.getItem('selected_post'));
 const post_div = document.createElement('div');
+post_div.classList.add('post');
 post_div.innerHTML = `<h2>Post ${post.id} details:</h2>
-                      <p>UserID:${post.userID}</p>
+                      <p>UserID:${post.userId}</p>
                       <p>Title:${post.title}</p>
                       <p>Body:${post.body}</p>`;
 document.body.appendChild(post_div);
@@ -10,13 +11,15 @@ async function load_comments(){
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`);
         const comments = await response.json();
         const div_for_comments = document.createElement('div');
+        div_for_comments.classList.add('comments_container')
         div_for_comments.innerHTML = `<h3>Post ${post.id} comments:</h3>`;
         comments.forEach(comment => {
             const comment_div = document.createElement('div');
-            comment_div.innerHTML = `<p>Comment's id: ${comment.id}</p>
-                                     <p>Name: ${comment.name}</p>
-                                     <p>Email: ${comment.id}</p>
-                                     <p>Comment's text: ${comment.body}</p>`;
+            comment_div.classList.add('comment');
+            comment_div.innerHTML = `<p><b>Comment's id:</b> ${comment.id}</p>
+                                     <p><b>Name:</b> ${comment.name}</p>
+                                     <p><b>Email:</b> ${comment.email}</p>
+                                     <p><b>Comment's text:</b> ${comment.body}</p>`;
             div_for_comments.appendChild(comment_div);
         });
         document.body.appendChild(div_for_comments);
--- user-details/user-details.js
+++ user-details/user-details.js
@@ -1,20 +1,21 @@
 const selected_user = JSON.parse(localStorage.getItem('selected_user'));
 const user_div = document.createElement('div');
+user_div.classList.add('user')
 user_div.innerHTML = `
         <h2>Name: ${selected_user.name}<br>ID:${selected_user.id}</h2>
         <p>Username: ${selected_user.username}</p>
         <p>Email: ${selected_user.email}</p>
-        <p>Address:</p>
+        <p><b>Address:</b></p>
         <p>Street: ${selected_user.address.street}</p>
         <p>Suite: ${selected_user.address.suite}</p>
         <p>City: ${selected_user.address.city}</p>
         <p>Zipcode: ${selected_user.address.zipcode}</p>
-        <p>Geo:</p>
+        <p><b>Geo:</b></p>
         <p>Lat: ${selected_user.address.geo.lat}</p>
         <p>Lng: ${selected_user.address.geo.lng}</p>
         <p>Phone: ${selected_user.phone}</p>
         <p>Website: ${selected_user.website}</p>
-        <p>Company:</p>
+        <p><b>Company:</b></p>
         <p>Company's name: ${selected_user.company.name}</p>
         <p>Company's catchphrase: ${selected_user.company.catchPhrase}</p>
         <p>Company's bs: ${selected_user.company.bs}</p>
@@ -22,15 +23,18 @@ user_div.innerHTML = `
 document.body.appendChild(user_div);
 
 const user_posts_button = document.createElement('button');
+user_posts_button.classList.add('reddit-mono');
 user_posts_button.innerText = 'Posts of current user';
 user_posts_button.addEventListener('click', async()=> {
     try{
         const response = await fetch(`https://jsonplaceholder.typicode.com/users/${selected_user.id}/posts`);
         const posts = await response.json();
         const div_for_posts = document.createElement('div');
+        div_for_posts.classList.add('post_container')
         div_for_posts.innerHTML = `<h3>Posts of ${selected_user.name}</h3>`
         posts.forEach(post => {
-            const postLink = document.createElement('a');
+            const postLink = document.createElement('div');
+            postLink.classList.add('post')
             postLink.addEventListener('click', ()=>{
                 localStorage.setItem('selected_post', JSON.stringify(post));
                 window.open(`../post-details/post-details.html?postId=${post.id}`, '_blank');
